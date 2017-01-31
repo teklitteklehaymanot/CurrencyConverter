a46eb36be05fc2af7e79fa73d489ef14e235ed68
titi <teklit@insa.et>
2017-01-31 05:06:39 -0800

    start our project

---
+++ calculation1/calculation1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculation1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("test 1");
+            Console.ReadLine();
+        }
+    }
+}
